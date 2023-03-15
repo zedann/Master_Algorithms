504118128033b944e09dea4fd43f5db6955b00f6
zedann <zedamohamed144@gmail.com>
2023-03-15 17:31:49 +0200

    sort

--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -28,10 +28,24 @@ void selection_sort(vector<int>&arr)
         swap(arr[i] , arr[min]);
     }
 }
+void count_sort(vector<int>&arr)
+{
+    //range [0,500] 
+    int mx_val = *max_element(arr.begin() , arr.end());
+    int freq[mx_val + 1] = {0};
+    for(auto i : arr) freq[i] ++;
+    int idx = 0;
+    for(int i =0; i <= mx_val ; i++)
+        if(freq[i] > 0)
+            for(int j = 0 ; j < freq[i] ; j++ , idx ++)
+                arr[idx] = i;
+        
+}
 int main(){
-    vector<int>arr = {9,2,10,0,5,3,90,85};
+    vector<int>arr = {9,2,10,0,5,10,3,90,85};
     //insertion_sort(arr);
-    selection_sort(arr);
+    // selection_sort(arr);
+    count_sort(arr);
     loop(arr.size()){
         cout << arr[i] << ' ';
     }
